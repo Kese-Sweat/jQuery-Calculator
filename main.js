a51dfd92f56d7765c2df6798bf70e367405ba474
Markese Straughter <Kese08@hotmail.com>
2020-03-17 16:40:46 -0700

    added sum

---
+++ main.js
@@ -0,0 +1,15 @@
+$('.buttonOne').click(function (){
+
+var num1 =  parseInt($('.numOne').val());
+var num2 =  parseInt($('.numTwo').val());
+var total= (num1 + num2) ;
+
+ $('.answer').val(total);
+
+
+})
+
+
+//Having difficulties trying to add function to button
+
+
