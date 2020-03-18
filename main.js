b0864cbb1bb51494bf633ce980c8c7f3ec1b9990
Markese Straughter <Kese08@hotmail.com>
2020-03-18 15:56:23 -0700

    Finished first challenge

--- main.js
+++ main.js
@@ -10,8 +10,64 @@ var total= (num1 + num2) ;
 })
 
 
+$('.buttonTwo').click(function (){
+
+    var num1 =  parseInt($('.numOne').val());
+    var num2 =  parseInt($('.numTwo').val());
+    var total= (num1 - num2) ;
+    
+     $('.answer').val(total);
+    
+    
+    })
+
+
+    $('.buttonThree').click(function (){
+
+        var num1 =  parseInt($('.numOne').val());
+        var num2 =  parseInt($('.numTwo').val());
+        var total= (num1 * num2) ;
+        
+         $('.answer').val(total);
+        
+        
+        })
+
+        $('.buttonFour').click(function (){
+
+            var num1 =  parseInt($('.numOne').val());
+            var num2 =  parseInt($('.numTwo').val());
+            var total= (num1 / num2) ;
+            
+             $('.answer').val(total);
+            
+            
+            })
+
+
+
+
+
+
+
+
+
 //Having difficulties trying to add function to button
 
 
 
-//How to make a grid?
+
+
+
+
+$(".number").click(function(event) {
+   $(".calculatorTwo.calcDisplay.value")
+   
+
+
+
+
+})
+
+
+// no clue on how to input the value in my calcDisplay
