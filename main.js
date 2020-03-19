cce8cb3a363ebdeb7d3656bd4836fa1cbec7e91d
Markese Straughter <Kese08@hotmail.com>
2020-03-18 21:39:44 -0700

    Finished calc2

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ $('.buttonTwo').click(function (){
             var num2 =  parseInt($('.numTwo').val());
             var total= (num1 / num2) ;
             
-             $('.answer').val(total);
+             $('.calcDisplay').val(total);
             
             
             })
@@ -52,22 +52,31 @@ $('.buttonTwo').click(function (){
 
 
 
-//Having difficulties trying to add function to button
+//Having difficulties trying to add function to button. Found out that i was writing the code wrong in JS
 
 
 
 
 
+// CREATED A FUNCTION THAT WILL DISPLAY VALUE
+
+function dis(val) 
+{ 
+    document.getElementById("result").value+=val 
+} 
+
+function solve()
+{ 
+    let x = document.getElementById("result").value 
+    let y = eval(x) 
+    document.getElementById("result").value = y 
+    // WILL RETURN RESULTS
+} 
+
+function clr()
+{ 
+    document.getElementById("result").value = "" 
+} 
 
 
-$(".number").click(function(event) {
-   $(".calculatorTwo.calcDisplay.value")
-   
-
-
-
-
-})
-
-
-// no clue on how to input the value in my calcDisplay
+// I DO NEED ASSISTANCE WITH USING THE "$()" SELECTOR, TO CALL A FUNCTION THAT WILL DISPLAY A VALUE
